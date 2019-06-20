541f5dc289408e50eb30c2e791af53f9e854eb25
edward0rtiz <931@holbertonschool.com>
2019-06-20 03:11:22 +0000

    Fixed code 101 v7

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- *main - print line
- *Return: 1
+ *main - Prints a string to standard error
+ *
+ *Description: Prints the string specified to the stderr pointer without using
+ *the function fprintf, puts, printf, or any of its derivatives
+ *Return: Returns the value 1
  */
-
 int main(void)
 {
-
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
