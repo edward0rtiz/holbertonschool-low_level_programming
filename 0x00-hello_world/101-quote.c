ceb3f88f9f8b951d0d4ea6ae334ad173fe11659a
edward0rtiz <931@holbertonschool.com>
2019-06-20 03:05:06 +0000

    Fixed code 101 v4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  */
 int main(void)
 {
-int l;
-l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+
+write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
