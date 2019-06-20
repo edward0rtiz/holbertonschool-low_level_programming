fce53884b46761de6fe88c0c90c169894ca13de2
edward0rtiz <931@holbertonschool.com>
2019-06-20 02:55:38 +0000

    Fixed code 101 v3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  *main - print line
@@ -6,8 +5,8 @@
  */
 int main(void)
 {
-int E;
-E = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", E);
+int l;
+l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
 return (1);
 }
