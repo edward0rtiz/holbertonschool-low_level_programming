e4ecc2534ca1f1c111f1b0ea3e43d68824343c9c
edward0rtiz <931@holbertonschool.com>
2019-06-20 02:33:44 +0000

    Add script 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints line
+ *Return: 1
+ */
+int main(void)
+{
+int l;
+l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+return (1);
+}
