2fc63a804ef71b0f9205619642bfa8ed3dd09af7
edward0rtiz <931@holbertonschool.com>
2019-06-20 23:25:28 +0000

    Add script 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description: print single number in base 10
+ * no char and no printf
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
