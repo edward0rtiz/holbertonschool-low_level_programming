1aaf38ccec505e9d3119732abda90950b5efce81
edward0rtiz <931@holbertonschool.com>
2019-06-22 03:16:38 +0000

    Fixed code 101 v2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 	int c;
 	int z = 0;
 
-	for (a = '0'; a <= '9'; a++)
+	for (a = '0'; a <= '7'; a++)
 	{
-		for (b = '0'; b <= '9'; b++)
+		for (b = '0'; b <= '8'; b++)
 		{
 			for (c = '0'; c <= '9'; c++)
 			{
