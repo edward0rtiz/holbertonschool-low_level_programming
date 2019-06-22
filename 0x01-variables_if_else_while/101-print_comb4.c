6b6c081501f9872948f04e6b94236fd560c3c66f
edward0rtiz <931@holbertonschool.com>
2019-06-22 03:12:57 +0000

    Fixed code 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * description: print differente combination of two digit-numbers
+ * description: print differente combination of three digit-numbers
  * Return: Always 0 (in process)
  */
 int main(void)
 {
-	int i, f;
+	int a;
+	int b;
+	int c;
+	int z = 0;
 
-	i = '0';
-	while (i <= '9')
+	for (a = '0'; a <= '9'; a++)
 	{
-		f = '0';
-		while (f <= '9')
+		for (b = '0'; b <= '9'; b++)
 		{
-			putchar(i);
-			putchar(f);
-			if ((i == '9') && (f == '9'))
+			for (c = '0'; c <= '9'; c++)
 			{
-				break;
+				if (a < b && b <= c && a != b && b != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					z++;
+					if (z < 126)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
 			}
-			else
-			{
-				putchar(',');
-				putchar(' ');
-			}
-			f++;
 		}
-		i++;
+		z++;
 	}
 	putchar('\n');
 	return (0);
