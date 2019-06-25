91dd45f6763240d986f539d8e8c990ae234ee9ad
edward0rtiz <931@holbertonschool.com>
2019-06-25 19:30:09 +0000

    added script 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
