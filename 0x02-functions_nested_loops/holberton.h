2be87db09ffaa4cfc226829c7a321d441e0f9275
edward0rtiz <931@holbertonschool.com>
2019-06-25 17:31:23 +0000

    Added script 9

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
+int main(void);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -6,3 +7,5 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
