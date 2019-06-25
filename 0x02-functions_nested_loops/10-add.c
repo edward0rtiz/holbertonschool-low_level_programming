c102533084a4826a6d7ff03e3de3cd1e1fd8da77
edward0rtiz <931@holbertonschool.com>
2019-06-25 17:40:06 +0000

    Added script 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - Function that adds two integers and returns the result.
+ * @a: type number
+ * @b: typee number
+ * Return: result of the addiion of two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
