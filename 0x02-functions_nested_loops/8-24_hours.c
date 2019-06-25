c355bd32310cac139f6c568ff9f22c62ce716bde
edward0rtiz <931@holbertonschool.com>
2019-06-25 15:34:50 +0000

    Add script 8 V2

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Print every minute of the day
+ * Description: Start from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+
+}
