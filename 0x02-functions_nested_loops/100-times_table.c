7b01c5932285c78decd4f410614ac3cf495125f5
edward0rtiz <931@holbertonschool.com>
2019-06-25 23:14:02 +0000

    Fixed code v5

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,43 +6,43 @@
  */
 void print_times_table(int n)
 {
-	int rw, clm, x, y, z, mul;
+	int x = 0, y, z;
 
-	if (n <= 15 && n >= 0)
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
 	{
-		for (rw = 0; rw <= n; rw++)
+		for (y = 0; y <= n; y++)
 		{
-			for (clm = 0; clm <= n; clm++)
+			z = x * y;
+			if (z > 99)
 			{
-				mul = rw + clm;
-				x = mul % 10;
-				y = mul / 10 % 10;
-				z = mul / 100;
-				if (z == 0 && clm != 0)
-				{
-					_putchar(' ');
-					if (y == 0)
-						_putchar(' ');
-					else
-						_putchar(y + '0');
-					_putchar(x + '0');
-				}
-				else if (z != 0)
-				{
-					_putchar(z + '0');
-					_putchar(y + '0');
-					_putchar(x + '0');
-				}
-				if (clm == 0)
-					_putchar('0');
-				if (clm != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
+				_putchar(z / 100 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
 			}
-			_putchar('\n');
-			clm = 0;
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('\n');
+		x++;
 	}
 }
