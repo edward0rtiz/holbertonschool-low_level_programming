698c6c68408357474e4aa568c86a0bf43a282b86
edward0rtiz <931@holbertonschool.com>
2019-06-25 19:52:52 +0000

    Fixed code 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,11 +5,11 @@
  * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
         int i;
 
-        i = 98;
+        i = 0;
         positive_or_negative(i);
 
         return (0);
