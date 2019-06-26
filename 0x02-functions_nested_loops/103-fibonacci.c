d2449106f7e74101413f444233d735cf92c16d22
edward0rtiz <931@holbertonschool.com>
2019-06-26 03:35:14 +0000

    Added script 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Fibonacci num not exceed 4000000
+ *
+ * return: return (0)
+ */
+int main(void)
+{
+	long int fb1 = 0;
+	long int fb2 = 1;
+	long int fb3 = 2;
+	long int sum;
+
+	while (fb3 < 4000000)
+	{
+		if (fb3 % 2 == 0)
+		{
+			fb1 += fb3;
+		}
+		sum = fb3;
+		fb3 += fb2;
+		fb2 = sum;
+	}
+	printf("%ld\n", fb1);
+	return (0);
+}
