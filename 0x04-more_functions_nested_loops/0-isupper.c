4ea1faebaad157a80c25f14d9420ef61843b5807
edward0rtiz <931@holbertonschool.com>
2019-06-27 21:14:11 +0000

    Fixed code v2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * int_isupper - check for the upper case characters.
- * @: type int character
+ * _isupper - check for the upper case characters.
+ * @c: type int character
  * Return:  1 if it is uppercase and 0 otherwise.
  */
 
