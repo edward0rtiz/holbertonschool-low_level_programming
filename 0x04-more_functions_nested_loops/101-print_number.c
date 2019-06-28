6407dc5fd339d6545e32d4d4d5eaa9112529bb71
edward0rtiz <931@holbertonschool.com>
2019-06-28 04:46:13 +0000

    Added script wp

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+/**
+ * print_number - Use to print an integer.
+ * @n: type int
+ * Description: not allowed arrays, long or pointers.
+ */
+void print_number(int n)
+{
+	int counter;
+ 	int num;
+	int bool;
+
+	counter = 1;
+	num = n;
+	bool = 1;
+
+	if (num < 0)
+	{
+	num *= -1;
+	_putchar('-');
+	}
+
+	while (bool)
+	{
+		if (num / ((long)counter * 10) > 0)
+			counter *= 10;
+		else
+		bool = 0;
+	}
+
+	while (num >= 0)
+	{
+		if (counter == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / counter % 10) + '0');
+			counter /= 10;
+		}
+	}
+}
