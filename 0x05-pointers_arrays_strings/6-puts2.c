e39c47b59bc300d1b7ef3c934c8d17918aa60219
edward0rtiz <931@holbertonschool.com>
2019-07-02 17:19:05 +0000

    Fixed code v2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,12 @@
 void puts2(char *str)
 {
 	int a;
-	char num;
 
-	for (a = 0; str[a] != 0; a++)
+	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (a % 2 == 0)
 		{
-			num = str[a];
-			_putchar(num);
+			_putchar(str[a]);
 		}
 	}
 	_putchar('\n');
