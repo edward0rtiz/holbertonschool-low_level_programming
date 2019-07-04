50f340de21b7c08f29db3730e293be2507c24517
edward0rtiz <931@holbertonschool.com>
2019-07-04 18:14:44 +0000

    Fixed code v4

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -15,10 +15,9 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; dest[a] != '\0'; a++)
 	{
 	}
-	for (b = 0; src[b] != '\0' && n > 0; b++, n--)
+	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
 	{
 		dest[a] = src[b];
-		a++;
 	}
 	return (dest);
 }
