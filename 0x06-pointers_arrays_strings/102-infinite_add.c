3c411a3b40dfc6e7eb92f55cda5979d7b8835cca
edward0rtiz <931@holbertonschool.com>
2019-07-09 13:50:24 +0000

    Updated work in progress

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -10,42 +10,41 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1, len2, bigger, modop, add = 0;
+	int len1, len2, strg, d2, d3, s3;
 
+	d3 = s3 = 0;
+/* defining length of n1 n2 with bigger */
 	for (len1 = 0; n1[len1] != '\0'; len1++)
-	{
-	}
+		;
 	for (len2 = 0; n2[len2] != '\0'; len2++)
-	{
-	}
+		;
+/* equal the longitude of len to buffer bigger */
 	if (len1 >= len2)
-		bigger = len1;
-	else
-		bigger = len2;
-	if (size_r <= bigger + 1)
+		strg = c1;
+	if (size_r <= strg)
 		return (0);
-
-	while (bigger > 02)
+	r [bg + 1]
+	len1 = len1 - 1;
+	len2 = len2 - 1;
+/* defining the longitude in 2 digits to store it in buffer */
+	while (len1 >= 0 || len2 >= 0)
 	{
-		if (len1 >= 0 && len2 >= 0)
+		d1 = d2 = 0;
+		if (len1 >= 0)
+			d1 = n1[len1--] - '0';
+		if (len2 >= 0)
+		d2 = n2[len2--] - '0';
+/* sum of d1 and d2 and the carry s3 */
+		d3 = d1 + d2 + s3;
+/* new condition */
+		if (d3 > 9)
 		{
-			modop = (n1[len1] - '0') + (n2[len2] - '0') + add;
+			s3 = 1;
+			s3 = s3 - 10;
 		}
 		else
-		{
-			if (len1 >= 0 && len2 < 0)
-				modop = (n2[len2] - '0') + add;
-			else
-			{
-				if (len1 < 0 && len2 >= 0)
-					modop = (n1[len1] - '0') + add;
-			}
-		}
-		*(r + bigger) = (modop % 10) + '0';
-		add = modop / 10;
-		len1--, len2--,	bigger--;
+			s3 = 0;
+r[]
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	return (r);
+		return (0);
 }
