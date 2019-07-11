3284d43df0e1080ada8d99f71d872374a28b40e0
edward0rtiz <931@holbertonschool.com>
2019-07-11 23:41:55 +0000

    [Fixed] code 100-wildcmp

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -15,5 +15,7 @@ int wildcmp(char *s1, char *s2)
 		return (0);
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 	return (0);
 }
