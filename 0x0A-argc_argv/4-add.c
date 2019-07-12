6348615dc55da07792878a3087a2c15f75ed285d
edward0rtiz <931@holbertonschool.com>
2019-07-12 22:18:45 +0000

    [Fixed]Fixed code v3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,34 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 /**
  * main - prints all arguments received.
  * @argc: type int argument
  * @argv: type char argument of string.
  * Return: if not receive 2 arg, rt error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int sum, val, x;
+	int sum, x;
 
-	sum = 0;
-	if (argc < 1)
-		printf("%d\n", 0);
-	while (argc-- && argc > 0)
+	(void)argv;
+	if (argc > 1)
 	{
-		for (x = 0; argv[argc][x] != '\0'; x++)
+		for (x = 1; x < argc; x++)
 		{
-
-			if (!(isdigit(argv[argc][x])))
+			if (atoi(argv[x]) != 0)
+				sum = sum + atoi(argv[x]);
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		val = atoi(argv[argc]);
-		sum = sum + val;
+		printf("%d\n", sum);
 	}
-	printf("%d\n", sum);
-	return (0);}
+	else if (argc == 1)
+	{
+		printf("0\n");
+	}
+	return (0);
+}
