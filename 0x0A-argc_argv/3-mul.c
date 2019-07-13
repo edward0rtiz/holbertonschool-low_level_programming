445fbb467ef7e86cc63f93cffd1ea1a350bf64b0
edward0rtiz <931@holbertonschool.com>
2019-07-13 22:43:00 +0000

    [Fixed]code3-mulv3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,7 +18,7 @@ int main(int argc, char *argv[])
 	{
 		n1 = atoi(argv[1]);
 		n2 = atoi(argv[2]);
-		mul =n1 * n2;
+		mul = n1 * n2;
 		printf("%d\n", mul);
 	}
 	return (0);
