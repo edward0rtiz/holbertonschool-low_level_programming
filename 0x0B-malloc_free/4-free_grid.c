d66c05a1782b5c63a302fc181a05b52a371ecaeb
edward0rtiz <931@holbertonschool.com>
2019-07-16 02:56:45 +0000

    [Add]code4v2

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,6 +5,7 @@
 /**
  * free_grid - free the 2 dimensional array of int using alloc_grid
  * @height: type int height.
+ * @grid: type int char
  * Return: Null if 0 or negative.
  */
 void free_grid(int **grid, int height)
