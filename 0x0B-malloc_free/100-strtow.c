b68e23a39fd6629f5c0790bf1addeefb0253dace
edward0rtiz <931@holbertonschool.com>
2019-07-17 19:06:35 +0000

    [Update]code100v2wp

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -4,7 +4,7 @@
 
 int wstrlen(char *str);
 int wcounter(char *str);
-
+char **strtow(char *str);
 /**
  * wstrlen - Calculates position of a word in a str
  * @str: type char str
@@ -16,7 +16,7 @@ int wstrlen(char *str)
 	int pos = 0;
 	int len = 0;
 
-	for (pos = 0; str[pos] && str[len] != '\0'; pos++, len++)
+	for (pos = 0; (*(str + pos)) && *(str + pos) != ' '; pos++, len++)
 		return (len);
 }
 
@@ -32,11 +32,11 @@ int wcounter(char *str)
 	int len = 0;
 	int w = 0;
 
-	for (pos = 0; str[pos]; pos++)
+	for (pos = 0; *(str + pos); pos++)
 		len++;
 	for (pos = 0; pos < len; pos++)
 	{
-		if (str[pos] != '\0')
+		if (*(str + pos) != ' ')
 		{
 			w++;
 			pos = pos + wstrlen(str + pos);
@@ -49,4 +49,42 @@ int wcounter(char *str)
  * @str: Type char str
  * Return: If str NULL or str = "" or function fails - NULL.
  */
-char **strtow(char *str)
+int **strtow(char *str)
+{
+	char **strs;
+	int pos = 0;
+	int len, len, w, x, y;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	w = wcounter(str);
+	if (w == 0)
+		return (NULL);
+
+	strs = malloc(sizeof(char *) * (w + 1));
+	if (strs == NULL)
+		return (NULL);
+
+	for (x = 0; x < w; x++)
+	{
+		while (str[pos] == ' ')
+			pos++;
+		Let = wstrlen(str + index);
+		strs[x] = malloc(sizeof(char) * (let + 1));
+
+		if (strs[x] == NULL)
+		{
+			for (; x >= 0; x--)
+				free(strs[x]);
+			free(strs);
+			return (NULL);
+		}
+		for (y = 0; y < let; y++)
+			(*(*(strs + x) + y)) = str[pos++];
+		(*(*(strs + x) + y)) = '\0';
+	}
+	strs[x] = NULL;
+
+	return (strs);
+}
