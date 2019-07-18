8985d8e62caabe1e749d882f057803b252d248e8
edward0rtiz <931@holbertonschool.com>
2019-07-18 18:54:52 +0000

    [Update]dir

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -5,4 +5,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _strlen(char *s);
 #endif
