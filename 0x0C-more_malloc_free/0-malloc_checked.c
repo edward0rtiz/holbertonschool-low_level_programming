af23e21a90968e915ace33b1f13d09e527785eb7
edward0rtiz <931@holbertonschool.com>
2019-07-18 15:16:44 +0000

    [Add]code0v1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * *malloc_checked - create array of char.
+ * @b: type unsigned int.
+ * Return: Pointer of allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
