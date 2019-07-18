91a9ac5701f7635b6c3a788b415553ac7374436a
edward0rtiz <931@holbertonschool.com>
2019-07-18 17:24:04 +0000

    [Add]code2v2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,8 +5,8 @@
 /**
  * _calloc - function that allocates memory for an array, using malloc
  * @nmemb: type unsigned int
- * @size: type unsigned int
-  * Return: NULL
+ * @size: type unsigned int size of elements
+ * Return: NULL
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
