6e3057ab6bb0430a4501eb1cc76934b2aecd4b68
edward0rtiz <931@holbertonschool.com>
2019-07-24 20:36:27 +0000

    [Fixed]3-main_change_name_var

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,30 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, total;
+	int num1, num2, calc;
 	int (*op_func)(int, int);
-	char *op;
+	char *operator;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(EXIT_FAILURE);
 	}
-	op = argv[2];
+	operator = argv[2];
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	if ((op[0] == '/' || op[0] == '%') && num2 == 0)
+	if ((operator[0] == '/' || operator[0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(EXIT_FAILURE);
 	}
-	op_func = get_op_func(op);
+	op_func = get_op_func(operator);
 	if (op_func == NULL)
 	{
 		printf("Error\n");
 		exit(EXIT_FAILURE);
 	}
-	total = op_func(num1, num2);
-	printf("%d\n", total);
+	calc = op_func(num1, num2);
+	printf("%d\n", calc);
 	exit(EXIT_SUCCESS);
 }
