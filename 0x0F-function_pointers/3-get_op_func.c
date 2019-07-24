87d86806e5723765478522ebb83dcdb3c3b2a38b
edward0rtiz <931@holbertonschool.com>
2019-07-24 20:44:29 +0000

    [Fixed]3-get_op_func_arraychange

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,11 @@ int (*get_op_func(char *s))(int, int)
 
 
 	i = 0;
-	while (i < 5)
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == s[0])
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
-		i++;
+		++i;
 	}
 	return (NULL);
 }
