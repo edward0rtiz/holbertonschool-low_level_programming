fa06b177094680ae5148fd844d90f90811ae66cb
edward0rtiz <931@holbertonschool.com>
2019-07-24 17:31:15 +0000

    [Update]Fixcode0nullarg_dir

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - Function that prints a name
  * @name: Type char string pointer
@@ -6,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL)
+		exit(EXIT_FAILURE);
 	f(name);
 }
