c4ee85b5d5f570c534e31a60b6fb58c98f7f2382
edward0rtiz <931@holbertonschool.com>
2019-07-24 20:18:58 +0000

    [Add]3-calc_3_get_op_3_op_3_main

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - return result of desired calculation from command line options
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: EXIT_FAILURE if unsuccessful or EXIT_SUCCESS if success.
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, total, j;
+	int (*op_func)(int, int);
+	char *op;
+
+	j = 0;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	op = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if ((*(op + j) == '/' || *(op + j) == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	op_func = get_op_func(op);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	total = op_func(num1, num2);
+	printf("%d\n", total);
+	exit(EXIT_SUCCESS);
+}
