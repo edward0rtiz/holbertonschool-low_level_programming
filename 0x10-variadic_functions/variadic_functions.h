df240dd34c631a4bea995515a1698327af9c8cbe
edward0rtiz <931@holbertonschool.com>
2019-07-26 21:51:09 +0000

    [Update]varadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifnndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
