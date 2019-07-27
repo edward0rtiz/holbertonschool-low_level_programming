cf32fd47c62329c4dc6ccbe07e8555d8afe2587e
edward0rtiz <931@holbertonschool.com>
2019-07-27 02:01:12 +0000

    [Fixed]change the va_list

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,23 +10,23 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list arg;
 	int sum;
 	unsigned int x;
-	va_list valist;
 
 	if (n == 0)
 		return (0);
 
 
-	va_start(valist, n);
+	va_start(arg, n);
 
 	sum = x = 0;
 	while (x < n)
 	{
-		sum += va_arg(valist, int);
+		sum += va_arg(arg, int);
 		x++;
 	}
 
-	va_end(valist);
+	va_end(arg);
 	return (sum);
 }
