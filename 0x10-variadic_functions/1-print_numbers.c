debe9f7f98bc6c0c875151a930e480f1d524fe95
edward0rtiz <931@holbertonschool.com>
2019-07-27 02:34:34 +0000

    [Fixed]change datay type U

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,16 +11,16 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
-	int x = n;
+	unsigned int x = n;
 
 	if (separator == NULL)
 		separator = "";
 
 	va_start(valist, n);
 	if (--x)
-		printf("%d", va_arg(valist, int));
+		printf("%d", va_arg(valist, unsigned int));
 	while (x-- > 0)
-		printf("%s%d", separator, va_arg(valist, int));
+		printf("%s%d", separator, va_arg(valist, unsigned int));
 	printf("\n");
 	va_end(valist);
 }
