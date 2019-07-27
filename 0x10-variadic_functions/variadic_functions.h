9cfa0c3e55c63d7b55eede103ebeb118295f1e40
edward0rtiz <931@holbertonschool.com>
2019-07-27 00:00:17 +0000

    [Update]Variadic_functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct print_all - search the func for print_all
+ * @c: character to match
+ * @f: function for that character
+ */
+typedef struct print
+{
+	char *c;
+	void (*f)();
+} print;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
