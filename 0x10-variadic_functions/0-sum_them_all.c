d055afdcb0bb92ffb74d46a92ace76bfc708d9a2
edward0rtiz <931@holbertonschool.com>
2019-07-27 02:30:21 +0000

    [Fix]original file while stmt

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,27 +3,28 @@
 
 /**
  * sum_them_all - func that returns the sum of all parameters.
- *
  * @n: type unsigned int
- * return: sum
+ * Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
-
 {
-	va_list arg;
 	int sum;
 	unsigned int x;
+	va_list valist;
 
-	sum = 0;
+
 	if (n == 0)
 		return (0);
 
-	va_start(arg, n);
+	va_start(valist, n);
 
-	for (x = 0; x < n; x++)
-		sum += va_arg(arg, int);
-
-	va_end(arg);
+	sum = x = 0;
+	while (x < n)
+	{
+		sum = sum + va_arg(valist, int);
+		x++;
+	}
+	va_end(valist);
 	return (sum);
 }
