62a22d89e02217a9749ff37bf1223a76fedefb99
edward0rtiz <931@holbertonschool.com>
2019-08-01 22:02:57 +0000

    [Add]code4 free list_t

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - Free a list_t list
+ * @head: Type pointer struct list_t
+ * Return: free head
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	tmp = head;
+	while (head != NULL)
+	{
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
+	free(head);
+}
