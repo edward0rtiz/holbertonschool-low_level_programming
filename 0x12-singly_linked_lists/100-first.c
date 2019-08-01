38dcb9506b66c11938dca751eb9198a5bf485e37
edward0rtiz <931@holbertonschool.com>
2019-08-01 22:14:59 +0000

    [add] 100 code added main before function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * before_main - function executed before main
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
