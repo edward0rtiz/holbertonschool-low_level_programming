df5ce3916b3e7afdcaa9395459fac69c571254d2
edward0rtiz <931@holbertonschool.com>
2019-08-06 21:35:45 +0000

    [fix]head to null

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -11,7 +11,7 @@ size_t free_listint_safe(listint_t **h)
 	listint_t *safe_node = *h;
 	listint_t *tmp_node;
 
-	while (safe_node != NULL)
+	while (!safe_node)
 	{
 		counter++;
 		tmp_node = safe_node;
