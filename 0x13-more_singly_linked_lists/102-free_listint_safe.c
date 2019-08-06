ee39a6baaa64d102bd57e3367204140c501796c8
edward0rtiz <931@holbertonschool.com>
2019-08-06 21:03:12 +0000

    [add]code102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: data type listint_t double pointer of head
+ * Return: the size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t counter;
+	listint_t *safe_node = *h;
+	listint_t *tmp_node;
+
+	for (; safe_node != NULL; counter++)
+	{
+		tmp_node = safe_node;
+		safe_node = safe_node->next;
+		free(tmp_node);
+
+		if (tmp_node < safe_node)
+			break;
+	}
+	*h = NULL;
+	return (counter);
+}
