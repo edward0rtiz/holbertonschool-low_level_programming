8076ec906e8f29951b1903e516d4ef55c9e16475
edward0rtiz <931@holbertonschool.com>
2019-08-07 03:11:06 +0000

    [fix]statemtes of first and second loop

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,22 +1,31 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a listint_t list
- * @h: data type listint_t double pointer of head
- * Return: the size of the list that was free'd
+ * find_listint_loop - find the loop in a linked list
+ * @head: data type listint_t double pointer of head
+ * Return: the loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *first_loop = head;
 	listint_t *second_loop = head;
 
-	while (first_loop && second_loop && second_loop->next)
+	if (head == NULL)
+		return  (NULL);
+	if (head == head->next)
+		return (head);
+	while (first_loop)
 	{
 		first_loop = first_loop->next;
-		second_loop = second_loop->next->next;
-
-		if (first_loop == second_loop)
-			return (first_loop);
+		second_loop = head;
+		while (first_loop && second_loop != first_loop)
+		{
+			if (second_loop == first_loop->next)
+			{
+				return (second_loop);
+			}
+			second_loop = second_loop->next;
+		}
 	}
-	return (NULL);
+	return (first_loop);
 }
