4c2c1a6bd9335b1afa4368ead9ece098ca4970c0
edward0rtiz <931@holbertonschool.com>
2019-08-08 20:43:23 +0000

    [add]set_bit left shift index OR=1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_bit - return the value of a bit in a given index
+ * @n: type unsigned long int input
+ * @index: type unsigned int position of unit
+ * Return: the value of a bit in an index or -1 if occurs
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	return (*n |= 1 << index);
+}
