628ac67f2ca33a21807ce89540ecc423845ab06f
edward0rtiz <931@holbertonschool.com>
2019-08-08 21:27:35 +0000

    [update]clear_bit function

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -13,6 +13,6 @@ void print_binary(unsigned long int n);
 void _right_pbin_mask(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
-
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
