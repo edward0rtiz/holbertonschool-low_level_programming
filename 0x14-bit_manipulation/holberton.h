cf3f7ab8ede9ef641e85bd5ef0558f719c10af77
edward0rtiz <931@holbertonschool.com>
2019-08-08 18:33:28 +0000

    [add]holberton.h

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+
+unsigned int binary_to_uint(const char *b);
+int _putchar(char c);
+
+#endif
