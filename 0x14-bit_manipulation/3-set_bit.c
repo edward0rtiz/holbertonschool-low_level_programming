fcd38a9caea82a0f0fe7499944c18acf9784662e
edward0rtiz <931@holbertonschool.com>
2019-08-08 20:48:00 +0000

    [fix]return 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 
 /**
- * get_bit - return the value of a bit in a given index
- * @n: type unsigned long int input
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: type unsigned long int input pointer
  * @index: type unsigned int position of unit
- * Return: the value of a bit in an index or -1 if occurs
+ * Return: 1 if works -1 if not
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
-	return (*n |= 1 << index);
+	*n |= 1 << index;
+	return (1);
 }
