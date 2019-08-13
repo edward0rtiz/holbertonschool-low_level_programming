1c642e44789c0949a68a1d2046ac53b6c725b81d
edward0rtiz <931@holbertonschool.com>
2019-08-13 23:52:07 +0000

    [add]3-cp betty check

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,8 +16,8 @@
 int main(int argc, char *argv[])
 {
 	char buffer[BUFFER_SIZE];
-	char *file_from;
-	char *file_to;
+	char *ff;
+	char *ft;
 	int fdread;
 	int fdwrite;
 	int cfile = 0;
@@ -28,18 +28,18 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	file_from = argv[1];
-	file_to = argv[2];
-	fdread = open(file_from, O_RDONLY);
+	ff = argv[1];
+	ft = argv[2];
+	fdread = open(ff, O_RDONLY);
 	if (fdread == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ff);
 		exit(98);
 	}
-	fdwrite = open(file_to, O_CREAT | O_TRUNC | O_WRONLY, 00664);
+	fdwrite = open(ft, O_CREAT | O_TRUNC | O_WRONLY, 00664);
 	if (fdwrite == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ft);
 		exit(99);
 	}
 	while ((rd = read(fdread, buffer, BUFFER_SIZE)))
