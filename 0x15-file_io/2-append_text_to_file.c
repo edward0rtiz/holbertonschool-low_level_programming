82e375c6135d4fe5dbfe95439b15bdefe2f1399a
edward0rtiz <931@holbertonschool.com>
2019-08-13 21:59:08 +0000

    [fix]change loop type

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,9 +19,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	return (-1);
 	if (text_content == NULL)
 		return (1);
-
-	for (x = 0; *(text_content + x) != '\0'; x++)
-		final_out = write(file, text_content, x);
+	while (*(text_content + x) != '\0')
+		x++;
+	final_out = write(file, text_content, x);
 	if (final_out == -1)
 		return (-1);
 	return (1);
