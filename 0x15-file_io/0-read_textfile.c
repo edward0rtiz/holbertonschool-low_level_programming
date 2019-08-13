c701a7c97086bc0220207b6abd6d6ddf9103947a
edward0rtiz <931@holbertonschool.com>
2019-08-13 19:08:50 +0000

    [add]code 0v1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: type char filename
+ * @letters: type size_t letters
+ * Return: if cannot be oppened or read 0, if is NULL 0, if writes fail 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	close (fd);
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	if (wr != rd)
+		return (0);
+	return (rd);
+}
