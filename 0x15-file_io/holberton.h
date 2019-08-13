b0a2eb4c36570979d81e7fa2e65f1bf11bccd9f0
edward0rtiz <931@holbertonschool.com>
2019-08-13 19:09:23 +0000

    [add] holberton.h

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
