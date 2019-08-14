0cecd6c5a3d4cdd1493f0aca1bdd8b53e5df445f
edward0rtiz <931@holbertonschool.com>
2019-08-14 01:50:22 +0000

    [fix]create close _w fiunt

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,23 @@
 
 #define BUFFER_SIZE 1024
 /**
+ * close_w - close function
+ * @fread: read
+ * @fwrite: write
+ */
+void close_w(int fdread, int fdwrite)
+{
+	if (close(fdwrite) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdwrite);
+		exit(100); }
+	if (close(fdread) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdread);
+		exit(100); }
+}
+
+/**
  * main - Function that copies the content of a file to another file
  * @argc: argument of count
  * @argv: argument of array
@@ -11,29 +28,26 @@ int main(int argc, char *argv[])
 {
 	char buffer[BUFFER_SIZE];
 	char *file_from, *file_to;
-	int fdread, fdwrite, cfile = 0;
+	int fdread, fdwrite;
 	ssize_t rd = 1024, wr;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		exit(97); }
 	file_from = argv[1];
 	file_to = argv[2];
 	fdread = open(file_from, O_RDONLY);
 	if (fdread == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		exit(98); }
 	fdwrite = open(file_to, O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
 	if (fdwrite == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
-	while ( rd == BUFFER_SIZE)
+		exit(99); }
+	while (rd == BUFFER_SIZE)
 	{
 		rd = read(fdread, buffer, BUFFER_SIZE);
 		if (rd == -1)
@@ -44,18 +58,7 @@ int main(int argc, char *argv[])
 		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-			exit(99);}}
-	if (close(fdwrite) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdwrite);
-		cfile = 1;
-	}
-	if (close(fdread) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdread);
-		cfile = 1;
-	}
-	if (cfile)
-		exit(100);
+			exit(99); }}
+	close_w(fdread, fdwrite);
 	return (0);
 }
