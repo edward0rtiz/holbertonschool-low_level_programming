b18a0bec16df46a07d8757f307dcde5ae04f954a
edward0rtiz <931@holbertonschool.com>
2019-08-18 20:25:24 +0000

    [update]test on logical operator &&

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int a;
 
-	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
 	{
 		if (s1[a] != s2[a])
 		return (s1[a] - s2[a]);
