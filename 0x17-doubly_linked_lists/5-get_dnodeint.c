a50dee601830112e47288a7c4947946744c1d447
edward0rtiz <931@holbertonschool.com>
2019-09-14 23:56:45 +0000

    [fix]ln 7 add return description

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,6 +4,7 @@
  * get_dnodeint_at_index - return the nth node of a dlistint list
  * @head: type pointer dlistint node
  * @index: type unsigned int index position of the node
+ * Return: return head
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
