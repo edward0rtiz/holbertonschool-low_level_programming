2448ce1a5241343e14e40df8d5a6910b7761eba7
edward0rtiz <931@holbertonschool.com>
2019-09-14 23:40:33 +0000

    [feature]add dnodeint

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add a anode at the beginning of a dlistint list
+ * @head: type dpointer of node
+ * @n: type int struct list is number
+ * return: &address of new element
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
+
+	return (new_node);
+}
