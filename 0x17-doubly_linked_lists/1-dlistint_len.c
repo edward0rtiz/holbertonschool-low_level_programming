e644966b45fe1db62d6ac86d6c2e013683ccb276
edward0rtiz <931@holbertonschool.com>
2019-09-14 23:39:56 +0000

    [feature]add dlistin_len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of elements in dlistint
+ * @h: type dlistint pointer of nodes
+ * return: n_nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n_nodes = 0;
+
+	while (h)
+	{
+		n_nodes++;
+		h = h->next;
+	}
+	return (n_nodes);
+}
