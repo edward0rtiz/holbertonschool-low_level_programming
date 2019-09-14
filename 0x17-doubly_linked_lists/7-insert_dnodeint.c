1d407479888273d6991b6808cafe8155a1e4be8b
edward0rtiz <931@holbertonschool.com>
2019-09-14 23:45:53 +0000

    [feature] add insert_dnodeint

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - return the nth node of a dlistint list
+ * @head: type pointer dlistint node
+ * @index: type unsigned int index position of the node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp_node = *h;
+	dlistint_t *new_node;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (; idx != 1; idx--)
+	{
+		temp_node = temp_node->next;
+		if (temp_node == NULL)
+			return (NULL);
+
+	}
+	if (temp_node->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = temp_node;
+	temp_node->next->prev = new_node;
+	temp_node->next = new_node;
+
+	return (new_node);
+}
