d26843025d89ff351991c933f624106975947b68
edward0rtiz <931@holbertonschool.com>
2019-09-14 23:38:50 +0000

    [feature]added printdlistint]

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print all elements of a dlistint.
+ * @h: type dlistint point of prev and next node
+ * return: n_nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n_nodes = 0;
+
+	while (h)
+	{
+		n_nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (n_nodes);
+}
