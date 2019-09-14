a72a607f87116ea62f0108f274f73ca73d8de9d8
edward0rtiz <931@holbertonschool.com>
2019-09-14 23:54:44 +0000

    [fix]ln 16 space before open parenthesis

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return(NULL);
+		return (NULL);
 
 	new_node->n = n;
 	new_node->next = NULL;
