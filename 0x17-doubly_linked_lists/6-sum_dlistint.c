8623311b19068e556e032843aa73dcd33ceb0d06
edward0rtiz <931@holbertonschool.com>
2019-09-15 00:01:34 +0000

    [fix]ln4 add function, ln6 add return, ln space required

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - return the nth node of a dlistint list
+ * sum_dlistint - return the nth node of a dlistint list
  * @head: type pointer dlistint node
- * @index: type unsigned int index position of the node
+ * Return: Return the sum of nodes
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int add = 0;
+
 	while (head)
 	{
 		add = add + head->n;
