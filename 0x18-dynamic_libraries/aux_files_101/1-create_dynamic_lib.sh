4e3c76fd5bbb02a057311573481e4de87316238d
edward0rtiz <931@holbertonschool.com>
2019-09-16 17:50:29 +0000

    [update]aux_files for 101 task

---
+++ 0x18-dynamic_libraries/aux_files_101/1-create_dynamic_lib.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -g -fPIC -Wall -Werror -Wextra -pedantic *.c -shared -o 100-operations.so
---
+++ 0x18-dynamic_libraries/aux_files_101/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - sum of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiply of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - module of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
