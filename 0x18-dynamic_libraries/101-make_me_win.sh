6dd95bab67db3e4286c0901790a2eebc25329020
edward0rtiz <931@holbertonschool.com>
2019-09-16 22:40:08 +0000

    [fix] code 101, add raw content

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so https://github.com/edward0rtiz/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/gigalib.so
+wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/edward0rtiz/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
 export LD_PRELOAD=/tmp/gigalib.so
