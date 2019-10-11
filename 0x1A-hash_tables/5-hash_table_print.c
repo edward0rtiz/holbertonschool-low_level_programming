d4463b9f450f75980ab6fd5186b9397c0aa6d2f1
edward0rtiz <931@holbertonschool.com>
2019-10-11 19:00:50 +0000

    [fix] ln 30 betty space

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -27,7 +27,7 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				printf("'%s': '%s'", current_node->key,
 				       current_node->value);
-				current_node =current_node->next;
+				current_node = current_node->next;
 				if (current_node != NULL)
 					printf(", ");
 			}
