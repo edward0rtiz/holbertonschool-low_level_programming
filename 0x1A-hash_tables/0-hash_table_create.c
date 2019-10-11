9915ec3cddafb7ad64badd073d95a25ee2f44e68
edward0rtiz <931@holbertonschool.com>
2019-10-11 15:13:41 +0000

    [fix] add the description

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 
 /**
- *
- *
- *
+ * hash_table_create - function to create a hash table
+ * @size: Size of the array
+ * Return: Null for error or a pointer to a new hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
