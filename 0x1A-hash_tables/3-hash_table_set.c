64353f37eb15db359064bddf09cf819fda231894
edward0rtiz <931@holbertonschool.com>
2019-10-11 19:30:03 +0000

    [fix] free ht in strcmp

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,6 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
+			free(ht->array[i]->value);
 			ht->array[i]->value = copy_val;
 			return (1);
 		}
