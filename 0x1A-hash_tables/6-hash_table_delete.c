70c2cb01e84c0d892ddce62c8272fdab928ad2f9
edward0rtiz <931@holbertonschool.com>
2019-10-11 19:30:22 +0000

    [fix] free head

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,8 +9,7 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_table_t *head = ht;
-	hash_node_t *node;
-	hash_node_t *temp;
+	hash_node_t *node, *temp;
 
 	for (i = 0; i < ht->size; i++)
 	{
