d1031b50fc2615cbd4bfc7ddf9b059735df8b527
edward0rtiz <931@holbertonschool.com>
2019-10-30 18:10:04 -0500

    [fix] fix indentation

--- 0x1B-sorting_algorithms/102-counting_sort.c
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 
-
 /**
  * counting_sort - Algorithm that sorts the array in order ascended
  *@array: type array of integers.
@@ -9,54 +8,44 @@
 
 void counting_sort(int *array, size_t size)
 {
-        int *count, *sort, sum, mx;
-        size_t i;
-	
-        if (array == NULL || size < 2)
-                return;
+	int *count, *sort, sum, mx;
+	size_t i;
 
-        sort = malloc(sizeof(int) * size);
-        if (sort == NULL)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        mx = array[0];
-        for (i = 1; i < size; i++)
-	{
-		if (array[i] > mx)
-		{
-			mx = array[i];
-		}
-		else
-		{
-			mx = mx;
-		}
-	}
+	sort = malloc(sizeof(int) * size);
+	if (sort == NULL)
+		return;
+
+	mx = array[0];
+	for (i = 1; i < size; i++)
+		mx = (array[i] > mx) ? array[i] : mx;
 	count = malloc(sizeof(int) * (mx + 1));
 	if (count == NULL)
 		return;
+
 	for (i = 0; i < (size_t)(mx + 1); i++)
-	{
 		count[i] = 0;
-	}
+
 	for (i = 0; i < size; i++)
-	{
 		count[array[i]]++;
-	}
+
 	for (i = 0, sum = 0; i < (size_t)(mx + 1); i++)
 	{
 		count[i] += sum;
 		sum = count[i];
 	}
 	print_array(count, mx + 1);
+
 	for (i = 0; i < size; i++)
 	{
 		sort[count[array[i]] - 1] = array[i];
 		count[array[i]]--;
 	}
 	for (i = 0; i < size; i++)
-	{
 		array[i] = sort[i];
-	}
+
 	free(sort);
 	free(count);
 }
