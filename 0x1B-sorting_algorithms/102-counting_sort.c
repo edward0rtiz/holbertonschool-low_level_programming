751bd570824494a9519755fd876f2dd877ac28aa
edward0rtiz <931@holbertonschool.com>
2019-10-30 17:50:52 -0500

    [feature] counting sor algotrithm test in progress

---
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+
+/**
+ * counting_sort - Algorithm that sorts the array in order ascended
+ *@array: type array of integers.
+ *@size: type size of array.
+ */
+
+void counting_sort(int *array, size_t size)
+{
+        int *count, *sort, sum, mx;
+        size_t i;
+	
+        if (array == NULL || size < 2)
+                return;
+
+        sort = malloc(sizeof(int) * size);
+        if (sort == NULL)
+                return;
+
+        mx = array[0];
+        for (i = 1; i < size; i++)
+	{
+		if (array[i] > mx)
+		{
+			mx = array[i];
+		}
+		else
+		{
+			mx = mx;
+		}
+	}
+	count = malloc(sizeof(int) * (mx + 1));
+	if (count == NULL)
+		return;
+	for (i = 0; i < (size_t)(mx + 1); i++)
+	{
+		count[i] = 0;
+	}
+	for (i = 0; i < size; i++)
+	{
+		count[array[i]]++;
+	}
+	for (i = 0, sum = 0; i < (size_t)(mx + 1); i++)
+	{
+		count[i] += sum;
+		sum = count[i];
+	}
+	print_array(count, mx + 1);
+	for (i = 0; i < size; i++)
+	{
+		sort[count[array[i]] - 1] = array[i];
+		count[array[i]]--;
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = sort[i];
+	}
+	free(sort);
+	free(count);
+}
