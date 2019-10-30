c61d8675d9e26434dd13163dd064c3148e005ae2
edward0rtiz <931@holbertonschool.com>
2019-10-30 18:11:45 -0500

    [update] sort.h

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -60,7 +60,7 @@ void merge_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void radix_sort(int *array, size_t size);
-
+void counting_sort(int *array, size_t size);
 
 
 #endif
