72971b6f47b357668b8880281f00eacdb7b46cbb
edward0rtiz <931@holbertonschool.com>
2019-10-29 20:21:50 -0500

    [feature] add cocktail algorithm

---
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -0,0 +1,97 @@
+#include "sort.h"
+
+/**
+ * swap_tail - function to swap list
+ *@list: list to be swapped
+ *@head: head of list
+ *@tail: tail of list
+ *
+ * Return - void
+ */
+void swap_tail(listint_t **list, listint_t **head, listint_t **tail)
+{
+	listint_t *temp = (*tail)->next;
+
+	if ((*tail)->prev != NULL)
+	{
+		(*tail)->prev->next = temp;
+	}
+	else
+		*list = temp;
+	temp->prev = (*tail)->prev;
+	(*tail)->next = temp->next;
+	if (temp->next != NULL)
+	{
+		temp->next->prev = *tail;
+	}
+	else
+		*head = *tail;
+	(*tail)->prev = temp;
+	temp->next = *tail;
+	*tail = temp;
+}
+
+/**
+ * swap_head - function to swap head of linked list
+ *@list: doubly linked list to be sorted
+ *@tail: tail of double linked list
+ *@head: head of double linked list
+ * Return - void
+ */
+void swap_head(listint_t **list, listint_t **tail, listint_t **head)
+{
+	listint_t *temp = (*head)->next;
+
+	if ((*head)->next != NULL)
+		(*head)->next->prev = temp;
+	else
+		*tail = temp;
+	temp->next = (*tail)->next;
+	(*head)->prev = temp->prev;
+	if (temp->prev != NULL)
+	{
+		temp->prev->next = *head;
+	}
+	else
+		*list = *head;
+	(*head)->next = temp;
+	temp->prev = *head;
+	*head = temp;
+}
+
+/**
+ * cocktail_sort_list - function to sort doubly linked list w/ cocktail sort
+ *@list: list to be sorted
+ * Return - void
+ */
+void cocktail_sort_list(listint_t **list)
+{
+	bool booln = false;
+	listint_t *head;
+	listint_t *tail;
+
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
+
+	for (; booln == false; booln = true)
+	{
+		for (head = *list; head != tail; head = head->next)
+		{
+			if (head->n > head->next->n)
+			{
+				swap_tail(list, &tail, &head);
+				print_list((const listint_t *) *list);
+				booln = false;
+			}
+		}
+		for (head = head->prev; head != *list; head = head->prev)
+		{
+			if (head->n < head->prev->n)
+			{
+				swap_head(list, &tail, &head);
+				print_list((const listint_t *) *list);
+				booln = false;
+			}
+		}
+	}
+}
