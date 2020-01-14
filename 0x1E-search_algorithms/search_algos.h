cb3f19e35f33b96d9ad76759aad21eb5ca6f884f
edward0rtiz <931@holbertonschool.com>
2020-01-14 18:08:27 -0500

    [update] search_algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -41,8 +41,9 @@ typedef struct skiplist_s
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
-/*int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_rec(int *array, size_t left, size_t right, int value);
+/*int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
