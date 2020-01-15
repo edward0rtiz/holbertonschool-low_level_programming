c04b7ba27bc4351bcc166ae44644b7eb5f617ebb
edward0rtiz <931@holbertonschool.com>
2020-01-15 17:08:21 -0500

    [fix] add another ternary at return ln 33

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -30,12 +30,5 @@ int jump_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-	if (array[i] == value)
-	{
-		return (i);
-	}
-	else
-	{
-		return (-1);
-	}
+	return (array[i] == value ? (int)i : -1);
 }
