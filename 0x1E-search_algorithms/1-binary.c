6d156cd634e1ce0716f2917b9f12954c7186fd9c
edward0rtiz <931@holbertonschool.com>
2020-01-15 14:26:13 -0500

    [fix] ln 22 space before searching text

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -19,7 +19,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = left; i < right; i++)
 			printf("%d, ", array[i]);
-		printf("%d\n ", array[i]);
+		printf("%d\n", array[i]);
 
 		i = left + (right - left) / 2;
 		if (array[i] == value)
