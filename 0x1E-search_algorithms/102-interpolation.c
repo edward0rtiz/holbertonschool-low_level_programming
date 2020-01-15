873084ccef296553082c2c38f272e4ad527bac55
edward0rtiz <931@holbertonschool.com>
2020-01-15 18:11:59 -0500

    [feature] interpolation applying formula

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - funct that searches for a value in a sorted array
+ * of int iterating using n jumps
+ * @array: Type pointer of given array
+ * @size: Type size of elements in the array
+ * @value: Type value to be searched
+ * Return: Value, or -1 if value not present
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t i, low, high;
+
+	if (!array)
+		return (-1);
+	for (low = 0, high = size - 1; high >= low;)
+	{
+		i = low + (((double)(high - low) / (array[high] - array[low])) *
+			   (value - array[i]));
+		if (i < size)
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		else
+		{
+			printf("Value checked array[%ld] is out of range\n", i);
+			break;
+		}
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+		{
+			high = i - 1;
+		}
+		else
+		{
+			low = i + 1;
+		}
+	}
+	return (-1);
+}
